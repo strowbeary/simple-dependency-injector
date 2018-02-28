ec73b1fed7ddb082c6aeecab510fd3e3dc03274d
StrowBeary <remicaillot5@gmail.com>
2018-02-28 16:55:01 +0100

    add tslint

--- lib/DependencyInjector.ts
+++ lib/DependencyInjector.ts
@@ -1,5 +1,5 @@
-let registredInjectables = new Array<string>();
-let injectables = new Map<String, any>();
+const registredInjectables: string[] = [];
+const injectables = new Map<string, any>();
 
 export function Injectable(target: any) {
     registredInjectables.push(target.name);
@@ -7,9 +7,9 @@ export function Injectable(target: any) {
 }
 
 export function Inject(providers: any[]) {
-    return function (constructor: any) {
-        let providersInstances: any = [];
-        for (let provider of providers) {
+    return (constructor: any) => {
+        const providersInstances: any = [];
+        for (const provider of providers) {
             if (injectables.get(provider.name) === void 0 && registredInjectables.includes(provider.name)) {
                 injectables.set(provider.name, new provider());
                 providersInstances.push(injectables.get(provider.name));
